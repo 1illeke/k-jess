41e7ea2542a5515f82bed699b1328459fd956252
Tristan Mõts <tristanmt14@gmail.com>
2025-08-03 20:22:57 +0300

    semi working names an chat, improved version in the works

--- backend/chatHandlers.js
+++
@@ -1,9 +0,0 @@
-export function setupChatHandlers(socket, io, games) {
-  socket.on('chat', ({ gameId, message, player }) => {
-    if (games[gameId]) {
-      const chatMsg = { id: Date.now(), player, message };
-      games[gameId].chat.push(chatMsg);
-      io.to(gameId).emit('chat', chatMsg);
-    }
-  });
-} 
