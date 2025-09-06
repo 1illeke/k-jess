62c5fcbc6203cc28771ed9896ce44ec234caa56d
Erik Tobias Loo <looerik04@gmail.com>
2025-09-07 00:58:41 +0300

    movement and rudimentary collision

--- apps/web/src/components/ui/boards/2player.jsx
+++ apps/web/src/components/ui/boards/2player.jsx
@@ -3,18 +3,29 @@ import './2player.css'
 import * as chess from '../../../chess.js'
 
 
-export default function TwoPlayerBoard({}) {
-  const [selected, setSelected] = useState(null)
+const DURATION = 3000.0
+const clamp = (n, min, max) => Math.min(Math.max(n, min),max)
+const lerp = (x, y, a) => x * (1 - a) + y * a;
 
-	let orientation = chess.Orientation.BOTTOM
+const orientation = chess.Orientation.BOTTOM
+
+export default function TwoPlayerBoard({
+	paused = false,
+	boardSize = 8,
+	orientation/* or team or whatever */= chess.Orientation.BOTTOM
+}) {
+	const largeBoard = boardSize === 14
+	const squareWidth = 1.0/boardSize*100
+
+  const [selected, setSelected] = useState(null)
 
 	let square_lookup = {}
 	const squares = []
 	{
-		for (let r = 0; r < 8; r++) {
+		for (let r = 0; r < boardSize; r++) {
 			let rank = []
-			for (let f = 0; f < 8; f++) {
-				let name = chess.getSquare(orientation, [f,r])
+			for (let f = 0; f < boardSize; f++) {
+				let name = chess.getSquare(orientation, [f,r], largeBoard)
 				square_lookup[name] = [f,r]
 				rank.push({
 					position: [f,r],
@@ -26,26 +37,32 @@ export default function TwoPlayerBoard({}) {
 		}
 	}
 
-	const [pieces, setPieces] = useState({
-		...chess.spawnPieces(chess.Orientation.BOTTOM),
-		...chess.spawnPieces(chess.Orientation.TOP),
-		c4:{
-			square:"c4",
-			team:chess.Orientation.TOP,
-			type:chess.Piece.ROOK
-		}
-	})
+	const [pieces, setPieces] = useState([
+		...chess.spawnPieces(chess.Orientation.TOP, largeBoard),
+		...chess.spawnPieces(chess.Orientation.BOTTOM, largeBoard),
+	])
 
-	// TODO needs to account for corners on 4v4
-	const in_bounds = v => Boolean(squares[v[0]] && squares[v[0]][v[1]])
-	const square_is_free = v => Boolean(in_bounds(v) && !pieces[chess.getSquare(orientation, v)])
-	const square_is_enemy = v => Boolean(in_bounds(v) && pieces[chess.getSquare(orientation, v)]?.team !== orientation)
+	const piece_lookup = {}
+	for (const piece of pieces) {
+		if (!piece.dead && piece.square) {
+			piece_lookup[piece.square] = piece
+		} else if (piece.animation && piece.team === orientation) {
+			piece_lookup[piece.animation.to] = piece // block moving to the same square
+			// TODO may need some tweaking
+		}
+	}
+
+	const piece_on_square = sq => piece_lookup[sq]
+	const in_corner = ([x,y]) => (x < 3 && y < 3) || (x < 3 && y > 10) || (x > 10 && y < 3) || (x > 10 && y > 10)
+	const in_bounds = v => Boolean(squares[v[1]] && squares[v[1]][v[0]] && (!largeBoard || !in_corner(v))) 
+	const square_is_free = v => Boolean(in_bounds(v) && !piece_on_square(squares[v[1]][v[0]].name))
+	const square_is_enemy = v => Boolean(in_bounds(v) && piece_on_square(squares[v[1]][v[0]].name)?.team !== orientation)
 
 	let moves = []
-	if (selected) {
+	if (selected && !selected.animation) {
 		let origin = square_lookup[selected.square]
 
-		function check(v, push_on_empty = true, push_on_enemy = true) {
+		function add_move_if_valid(v, push_on_empty = true, push_on_enemy = true) {
 			const p = chess.add(origin, v)
 
 			if (square_is_free(p)) {
@@ -53,22 +70,21 @@ export default function TwoPlayerBoard({}) {
 				return true
 			}
 
-			if (square_is_enemy(p)) {
-				if (push_on_enemy) moves.push(p)
-			}
+			if (square_is_enemy(p) && push_on_enemy) moves.push(p)
 			return false
 		}
 
 		switch (selected.type) {
 			case chess.Piece.PAWN:
-				check([0,-1], true, false) && !selected.hasMoved && check([0,-2], true, false)
-				check([1,-1], false, true)
-				check([-1,-1], false, true)
+				add_move_if_valid([0,-1], true, false) &&
+					!selected.hasMoved && add_move_if_valid([0,-2], true, false) // 1st move can be 2 squares
+				add_move_if_valid([1,-1], false, true)
+				add_move_if_valid([-1,-1], false, true)
 				break
 			case chess.Piece.ROOK:
 				for (let dir of [[0,1],[1,0],[-1,0],[0,-1]]) {
 					let range = 1
-					while (check(chess.mul(dir, range))) {
+					while (add_move_if_valid(chess.mul(dir, range))) {
 						range += 1
 					}
 				}
@@ -76,7 +92,7 @@ export default function TwoPlayerBoard({}) {
 			case chess.Piece.BISHOP:
 				for (let dir of [[-1,1],[1,-1],[-1,-1],[1,1]]) {
 					let range = 1
-					while (check(chess.mul(dir, range))) {
+					while (add_move_if_valid(chess.mul(dir, range))) {
 						range += 1
 					}
 				}
@@ -84,7 +100,7 @@ export default function TwoPlayerBoard({}) {
 			case chess.Piece.QUEEN:
 				for (let dir of [[-1,1],[1,-1],[-1,-1],[1,1],[0,1],[1,0],[-1,0],[0,-1]]) {
 					let range = 1
-					while (check(chess.mul(dir, range))) {
+					while (add_move_if_valid(chess.mul(dir, range))) {
 						range += 1
 					}
 				}
@@ -92,81 +108,178 @@ export default function TwoPlayerBoard({}) {
 			case chess.Piece.KING:
 				for (let dir of [[-1,1],[1,-1],[-1,-1],[1,1],[0,1],[1,0],[-1,0],[0,-1]]) {
 					let range = 1
-					check(chess.mul(dir, range))
+					add_move_if_valid(chess.mul(dir, range))
 				}
 				break
 			case chess.Piece.KNIGHT:
 				for (let point of [[1,2],[1,-2],[2,1],[2,-1]]) {
-					check(point)
-					check(chess.neg(point))
+					add_move_if_valid(point)
+					add_move_if_valid(chess.neg(point))
 				}
 				break
 		}
 	}
-	const clamp = (n, min, max) => Math.min(Math.max(n, min),max)
 
+	// strict mode loop guard
+	const [looping, setLooping] = useState(false)
 	async function frame() {
-		return
 		// https://x.com/its_bvisness/status/1960370065284460550
+		let lastFrame = performance.now()
 		while (true) {
 			await new Promise(requestAnimationFrame)
-			pieces.map(() => {
-				if (!piece.target) return piece
+			let delta = performance.now() - lastFrame
+			lastFrame = performance.now()
 
-				return piece
+			if (window.paused) continue
+
+			setPieces(pieces => {
+				let i = -1
+				for (const piece of pieces) {
+					i += 1
+					if (!piece.animation || piece.dead) continue
+
+					// collisions
+					for (let j = 0; j < pieces.length; j += 1) {
+						if (i == j) continue
+						let other = pieces[j]
+						if (other.dead) continue
+						if (other.team === orientation) continue
+						else {
+							const other_is_moving = other.animation
+							let other_position
+							if (!other_is_moving) {
+								other_position = square_lookup[other.square]
+							} else {
+								other_position = other.position
+							}
+							let diff = chess.abs(chess.sub(piece.position, other_position))
+							const length = ([x,y]) => Math.sqrt(x*x+y*y)
+							const distance = length(diff)
+							// TODO tweak distance and check for king?
+							if (distance < 0.5) {
+								if (!other_is_moving) {
+									other.dead = true
+								} else {
+									// TODO test moving kill code when multiplayer works
+									// TODO should this just be time based or should the strongest piece survive
+									// I think kfchess.com does this
+									if (other.animation.time < piece.animation.time) {
+										other.dead = true
+									} else {
+										piece.dead = true
+									}
+								}
+							}
+						}
+					}
+
+					piece.animation.time += delta
+					const elapsed = clamp(
+						piece.animation.time/DURATION,
+						0.0, 1.0)
+
+					let [x, y] = square_lookup[piece.animation.from]
+					const [tx, ty] = square_lookup[piece.animation.to]
+					x = lerp(x, tx, elapsed)
+					y = lerp(y, ty, elapsed)
+
+					piece.position = [x, y]
+
+					const animation_finished = elapsed === 1
+					if (animation_finished) {
+						delete piece.animation
+						piece.position = chess.round(piece.position)
+						let [x, y] = piece.position
+						piece.square = squares[y][x].name
+
+						// promotion
+						if (piece.type === chess.Piece.PAWN && y <= boardSize-8) {
+							piece.type = chess.Piece.QUEEN
+						}
+					}
+				}
+				return [...pieces]
 			})
-			setPieces(pieces)
 		}
 	}
-	useEffect(()=>{frame()}, [])
+
+	useEffect(()=>{
+		if (looping) return
+		setLooping(true)
+		frame()
+	}, [])
+
 
   return <div className="chess-board">
-			<div className="chess-squares">
+			<div className="chess-squares" style={{'--grid-size': boardSize}}>
 			{squares.flat().map((square, i) => {
-				return (<div
+				let classes = ['chess-square']
+				// don't color corners
+				if (!largeBoard || in_bounds(square.position)) {
+					classes.push(square.white ? 'light' : 'dark')
+				}
+				return <div
 				key={i}
-				className={[ 'chess-square', square.white ? 'light' : 'dark'
-				].filter(Boolean).join(' ')}>
-					{square.name}
-				</div>)
+				className={classes.filter(Boolean).join(' ')}>
+				</div>
 			})}
 			</div>
 
-			{Object.keys(pieces).map(square => {
-				const p = pieces[square]
+			{pieces.map((piece, i) => {
+				if (piece.dead) return
 				const click = () => {
-					if (selected?.square === square) {
+					if (piece.animation) return
+					if (selected?.square === piece.square) {
 						setSelected(null)
 					} else {
-						setSelected(p)
+						setSelected(piece)
 					}
 				}
 
-				let [x, y] = square_lookup[p.square]
+				const myPiece = piece.team === orientation
+				let [x, y] = piece.position || square_lookup[piece.square]
 				return (<img
-					onClick={click}
-					src={chess.filenames[p.team][p.type]}
-					key={square}
-					className={`chess-piece ${selected?.square === square? 'selected':''}`}
-					style={{ left: `${x*12.5}%`, top: `${y*12.5}%`}}
+					draggable="false"
+					onClick={myPiece? click : null}
+					src={chess.filenames[piece.team][piece.type]}
+					key={i}
+					className={`chess-piece ${selected?.square === piece.square? 'selected':''}`}
+					style={{ left: `${x*squareWidth}%`, top: `${y*squareWidth}%`,
+							width: `${squareWidth}%`,
+							height: `${squareWidth}%`,
+					}}
 				/>)
 			})}
 
 			{moves.map((m,i) => {
-				const click = () => {
+				const [x, y] = m
+				const click = (e) => {
+					if (selected.animation) return
+					let from = selected.square
+					setSelected(null)
 					setPieces(prev => {
-						prev[selected.square].target = m
-						prev.start = performance.now()
-						prev.end = performance.now() + 3000
+						const piece = piece_on_square(from)
+						if (piece.square) {
+							piece.position = square_lookup[piece.square]
+							piece.hasMoved = true
+							piece.animation = {
+								from: piece.square,
+								to: squares[y][x].name,
+								time: 0
+							}
+							delete piece.square
+						}
 						return prev
 					})
 				}
-				const [x, y] = m
 				return <div
 					onClick={click}
 					key={i}
 					className="move"
-					style={{ left: `${x*12.5}%`, top: `${y*12.5}%` }}
+					style={{ left: `${x*squareWidth}%`, top: `${y*squareWidth}%`,
+							width: `${squareWidth}%`,
+							height: `${squareWidth}%`,
+					}}
 					/>
 			})}
 		</div>
