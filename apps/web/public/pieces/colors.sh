7df03cbe23e8ee96747fe42c9c7acc508bda314a
Erik Tobias Loo <looerik04@gmail.com>
2025-09-17 14:46:18 +0300

    svg

--- apps/web/public/pieces/colors.sh
+++ apps/web/public/pieces/colors.sh
@@ -1,9 +1,9 @@
 
-COLOR=#ff0000
-ORIENTATION=left
+COLOR=#eeeeee
+ORIENTATION=bottom
 
-for file in bottom_*.svg
+for file in base_*.svg
 do
-	new_file=${file/bottom_/${ORIENTATION}_}
+	new_file=${file/base_/${ORIENTATION}_}
 	sed "s/fill:#ffffff/fill:$COLOR/" $file > $new_file
 done
